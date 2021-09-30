9d3f10c576f8339696b6d175fe403008384ed2ab
Svetlana K <>
2021-09-30 17:05:21 +0300

    Класс с параметрами

---
+++ contact.py
@@ -0,0 +1,21 @@
+class Contact:
+    def __init__(self, firstname, middlename, lastname, nickname, Company, titlen, address, mobile, homephone,
+                    mail, bday, bmouth, byear, address2, notes, phone2):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.Company = Company
+        self.titlen = titlen
+        self.address = address
+        self.mobile = mobile
+        self.homephone = homephone
+        self.mail = mail
+        self.bday = bday
+        self.bmouth = bmouth
+        self.byear = byear
+        self.address2 = address2
+        self.notes = notes
+        self.phone2 = phone2
+
+
--- test_add_contact.py
+++ test_add_contact.py
@@ -5,6 +5,7 @@ from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest
+from contact import Contact
 
 
 class TestAddContact(unittest.TestCase):
@@ -16,70 +17,69 @@ class TestAddContact(unittest.TestCase):
         dv = self.dv
         self.open_page(dv)
         self.login(dv, username="admin", password="secret")
-        self.add_contact(dv, firstname = "Svetlana", middlename="khsdks", lastname="KKKKKK", nickname="Nick", Company="Compa", titlen="Title", address="Add", mobile="+75656567676",
-                         homephone="+76565656767", mail="gdesveta@noya.ru", bday="26", bmouth="August", byear="1990", address2="addr", notes="hkda", phone2="khad")
+        self.add_contact(dv, Contact(firstname="Svetlana", middlename="khsdks", lastname="KKKKKK", nickname="Nick", Company="Compa", titlen="Title", address="Add", mobile="+75656567676",
+                         homephone="+76565656767", mail="gdesveta@noya.ru", bday="26", bmouth="August", byear="1990", address2="addr", notes="hkda", phone2="khad"))
         self.logout(dv)
 
     def logout(self, dv):
         dv.find_element_by_link_text("Logout").click()
 
-    def add_contact(self, dv, firstname, middlename, lastname, nickname, Company, titlen, address, mobile, homephone,
-                    mail, bday, bmouth, byear, address2, notes, phone2):
+    def add_contact(self, dv, contact):
         # fill form
         dv.find_element_by_name("theform").click()
         dv.find_element_by_name("firstname").click()
         dv.find_element_by_name("firstname").clear()
-        dv.find_element_by_name("firstname").send_keys(firstname)
+        dv.find_element_by_name("firstname").send_keys(contact.firstname)
         dv.find_element_by_name("theform").click()
         dv.find_element_by_name("middlename").click()
         dv.find_element_by_name("middlename").clear()
-        dv.find_element_by_name("middlename").send_keys(middlename)
+        dv.find_element_by_name("middlename").send_keys(contact.middlename)
         dv.find_element_by_name("lastname").click()
         dv.find_element_by_name("lastname").clear()
-        dv.find_element_by_name("lastname").send_keys(lastname)
+        dv.find_element_by_name("lastname").send_keys(contact.lastname)
         dv.find_element_by_name("nickname").click()
         dv.find_element_by_name("nickname").clear()
-        dv.find_element_by_name("nickname").send_keys(nickname)
+        dv.find_element_by_name("nickname").send_keys(contact.nickname)
         dv.find_element_by_name("company").click()
         dv.find_element_by_name("company").clear()
-        dv.find_element_by_name("company").send_keys(Company)
+        dv.find_element_by_name("company").send_keys(contact.Company)
         dv.find_element_by_name("theform").click()
         dv.find_element_by_name("title").click()
         dv.find_element_by_name("title").clear()
-        dv.find_element_by_name("title").send_keys(titlen)
+        dv.find_element_by_name("title").send_keys(contact.titlen)
         dv.find_element_by_name("address").click()
         dv.find_element_by_name("address").clear()
-        dv.find_element_by_name("address").send_keys(address)
+        dv.find_element_by_name("address").send_keys(contact.address)
         dv.find_element_by_name("mobile").click()
         dv.find_element_by_name("mobile").clear()
-        dv.find_element_by_name("mobile").send_keys(mobile)
+        dv.find_element_by_name("mobile").send_keys(contact.mobile)
         dv.find_element_by_name("home").click()
         dv.find_element_by_name("home").clear()
-        dv.find_element_by_name("home").send_keys(homephone)
+        dv.find_element_by_name("home").send_keys(contact.homephone)
         dv.find_element_by_xpath("//div[@id='content']/form/label[14]").click()
         dv.find_element_by_name("email").click()
         dv.find_element_by_name("email").clear()
-        dv.find_element_by_name("email").send_keys(mail)
+        dv.find_element_by_name("email").send_keys(contact.mail)
         dv.find_element_by_name("theform").click()
         dv.find_element_by_name("bday").click()
-        Select(dv.find_element_by_name("bday")).select_by_visible_text(bday)
+        Select(dv.find_element_by_name("bday")).select_by_visible_text(contact.bday)
         dv.find_element_by_xpath("//option[@value='26']").click()
         dv.find_element_by_name("bmonth").click()
-        Select(dv.find_element_by_name("bmonth")).select_by_visible_text(bmouth)
+        Select(dv.find_element_by_name("bmonth")).select_by_visible_text(contact.bmouth)
         dv.find_element_by_xpath("//option[@value='August']").click()
         dv.find_element_by_name("byear").click()
         dv.find_element_by_name("byear").clear()
-        dv.find_element_by_name("byear").send_keys(byear)
+        dv.find_element_by_name("byear").send_keys(contact.byear)
         dv.find_element_by_name("theform").click()
         dv.find_element_by_name("address2").click()
         dv.find_element_by_name("address2").clear()
-        dv.find_element_by_name("address2").send_keys(address2)
+        dv.find_element_by_name("address2").send_keys(contact.address2)
         dv.find_element_by_name("notes").click()
         dv.find_element_by_name("notes").clear()
-        dv.find_element_by_name("notes").send_keys(notes)
+        dv.find_element_by_name("notes").send_keys(contact.notes)
         dv.find_element_by_name("phone2").click()
         dv.find_element_by_name("phone2").clear()
-        dv.find_element_by_name("phone2").send_keys(phone2)
+        dv.find_element_by_name("phone2").send_keys(contact.phone2)
         # submit contact
         dv.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
 
