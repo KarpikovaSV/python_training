17360cf77e1410a51c3d766bf0830bca1d8347de
Svetlana K <>
2021-10-05 17:04:00 +0300

    класс для работы с группами

--- fixture/application.py
+++ fixture/application.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 from fixture.session import SessionHelper
+from fixture.group import GroupHelper
 
 
 class Application:
@@ -9,30 +10,7 @@ class Application:
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
         self.session = SessionHelper(self)
-
-    def return_group_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("groups").click()
-
-    def create_group(self, group):
-        wd = self.wd
-        # init group creation
-        self.open_groups_page()
-        wd.find_element_by_name("new").click()
-        # fill group
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # submit group creation
-        wd.find_element_by_name("submit").click()
-        self.return_group_page()
-
+        self.group = GroupHelper(self)
     def open_groups_page(self):
         wd = self.wd
         wd.find_element_by_link_text("groups").click()
@@ -40,7 +18,6 @@ class Application:
     def open_home_page(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/index.php")
-
     def is_element_present(self, how, what):
         try:
             self.wd.find_element(by=how, value=what)
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -13,11 +13,11 @@ def app(request):
 
 def test_add_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(header="jnjhn", name="bjbjjhh", footer="nm"))
+    app.group.create(Group(header="jnjhn", name="bjbjjhh", footer="nm"))
     app.session.logout()
 
 
 def test_add_empty_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(header="", name="", footer=""))
+    app.group.create(Group(header="", name="", footer=""))
     app.session.logout()
